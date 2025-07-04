d5651d2db948df3966e0733420401149b5911e3d
N1200161 <israel.elulade2023@my.ntu.ac.uk>
2025-07-05 00:02:41 +0100

    found the cumulative returns from the log of daily close prices, used this to create plot shoiwng trajectory

--- spa.py.py
+++ spa.py.py
@@ -2,17 +2,33 @@ import yfinance as yf
 import pandas as pd
 import datetime as dt
 import numpy as np
+import matplotlib.pyplot as plt
+#Libraries used in this project
 
 
 endDate = dt.datetime.now()
 startDate = endDate - dt.timedelta(days = 365*5)
 
+#The datetime function allows us to specifically select a period of time
+
 stocks = ['MSFT','NVDA' ,'NTDOY' ,'AAPL']
 df = yf.download(stocks, start = startDate, end = endDate, auto_adjust=True)
 
+#Created a list of stocks I am using, and used yfinance to doownload their details and assigned it to a dataframe
 
 close_Prices = df['Close']
 log_DReturns = np.log(close_Prices/close_Prices.shift(1))
 
+#made a seperate dateframe for their close prices
 
-print (log_DReturns.head())
+#divided the prices from the day before and found the log of that value 
+#first row divded by nothing thats why it's not accessible
+
+log_CumuReturns = log_DReturns.cumsum()
+
+#Each day takes the previous value, adds it to the next day and stores it,
+# cycles thorugh add the next value and will either increase or drcease over time 
+
+log_CumuReturns.plot(title = 'Cumulative Returns',figsize = (10,6))
+
+plt.show()
