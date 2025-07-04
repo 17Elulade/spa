262c42cb2286333d0b14f7a02f337e0879a184f6
N1200161 <israel.elulade2023@my.ntu.ac.uk>
2025-07-04 22:14:02 +0100

    daily returns calc done

--- spa.py.py
+++ spa.py.py
@@ -8,10 +8,11 @@ endDate = dt.datetime.now()
 startDate = endDate - dt.timedelta(days = 365*5)
 
 stocks = ['MSFT','NVDA' ,'NTDOY' ,'AAPL']
-
 df = yf.download(stocks, start = startDate, end = endDate, auto_adjust=True)
 
 
 close_Prices = df['Close']
+log_DReturns = np.log(close_Prices/close_Prices.shift(1))
 
-print (close_Prices.head())
+
+print (log_DReturns.head())
